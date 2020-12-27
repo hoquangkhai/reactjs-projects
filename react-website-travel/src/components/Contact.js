c5218772e9f8c506e0b29f82f9e23725bbb35d0a
Pandora-86 <quangkhai846@gmail.com>
2020-12-27 08:01:08 +0700

    prompt: thong bao khi muon chuyen trang

--- react-website-travel/src/components/Contact.js
+++ react-website-travel/src/components/Contact.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { Prompt } from "react-router-dom";
 
 function Contact() {
-  return <div>day la trang Contact</div>;
+  const [ischecked, setIsChecked] = useState(false);
+  const handleClick = (boolean) => {
+    setIsChecked(boolean);
+  };
+
+  return (
+    <div>
+      day la trang Contact
+      <button className="btn--green" onClick={() => handleClick(false)}>
+        Chuyen trang khong can hoi
+      </button>
+      <button className="btn--red" onClick={() => handleClick(true)}>
+        Chuyen trang can thong bao truoc
+      </button>
+      <Prompt
+        when={ischecked}
+        message={(locaition) => `Ban co chac muon di den ${locaition.pathname}`}
+      />
+    </div>
+  );
 }
 
 export default Contact;
